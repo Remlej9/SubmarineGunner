2294a1ae5559cad1606d9bffc91cfd6e3332c079
Remlej9 <Remlej9@gmail.com>
2021-03-29 18:33:44 +0200

    Update bot.js

--- src/bot.js
+++ src/bot.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
 const Discord = require('discord.js');
-const client = new Discord.client();
+const client = new Discord.Client();
 
 client.on("ready", () => {
 	console.log("Hello World");
