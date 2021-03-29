35466fd7b29b24a2774403676046d0e6ba61afba
Remlej9 <Remlej9@gmail.com>
2021-03-29 19:02:13 +0200

    Update bot.js

--- src/bot.js
+++ src/bot.js
@@ -4,10 +4,10 @@ const client = new Discord.Client();
 
 client.on("ready", () => {
 	console.log("Hello World");
-	client.user.setActivity('submarineguns', { type: 'WATCHING' })
+	Client.user.setActivity('submarineguns', { type: 'WATCHING' })
   .then(presence => console.log(`Activity set to ${presence.activities[0].name}`))
   .catch(console.error);
-  var guild = client.guilds.get('590635305137078287');
+  var guild = Client.guilds.get('590635305137078287');
   if(guild && guild.channels.get('channelid')){
 		    guild.channels.cache.get('826133568148602931').send({
 		files: ['https://i.imgur.com/Tivrl2P.jpg']
