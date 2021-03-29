08793e8aa2d6444a89be15657e92fb6a98fd6eda
Remlej9 <Remlej9@gmail.com>
2021-03-29 18:49:09 +0200

    Update bot.js

--- src/bot.js
+++ src/bot.js
@@ -7,7 +7,9 @@ client.on("ready", () => {
 	client.user.setActivity('submarineguns', { type: 'WATCHING' })
   .then(presence => console.log(`Activity set to ${presence.activities[0].name}`))
   .catch(console.error);
-    client.channels.cache.get('826133568148602931').send('Hello here!');
+    client.channels.cache.get('826133568148602931').send('Hello here!')
+	.then(message => console.log(`Sent message: ${message.content}`))
+  .catch(console.error);
 });
 
 client.login(process.env.BOT_TOKEN);
