aca3ad11f6981c975c2e0581840b0f663727f420
Remlej9 <Remlej9@gmail.com>
2021-03-29 18:52:57 +0200

    Update bot.js

--- src/bot.js
+++ src/bot.js
@@ -7,8 +7,10 @@ client.on("ready", () => {
 	client.user.setActivity('submarineguns', { type: 'WATCHING' })
   .then(presence => console.log(`Activity set to ${presence.activities[0].name}`))
   .catch(console.error);
-    client.channels.cache.get('826133568148602931').send('Hello here!')
-	.then(message => console.log(`Sent message: ${message.content}`))
+    client.channels.cache.get('826133568148602931').send({
+		files: ['https://i.imgur.com/Tivrl2P.jpg']
+	})
+	.then(message => console.log(`Sent submarinegun`))
   .catch(console.error);
 });
 
