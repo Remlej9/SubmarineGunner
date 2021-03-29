d99bb4981372f11c1829bf898acfdcafe4e8623c
Remlej9 <Remlej9@gmail.com>
2021-03-29 19:43:12 +0200

    Images

--- src/bot.js
+++ src/bot.js
@@ -2,6 +2,23 @@ require('dotenv').config();
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
+var images = [
+	'https://imgur.com/E9I7vt0.jpg',
+	'https://imgur.com/piYVrpR.jpg',
+	'https://imgur.com/0pVaIp9.jpg',
+	'https://imgur.com/tZX8sar.jpg',
+	'https://imgur.com/O8tHGFG.jpg',
+	'https://imgur.com/SBP40yW.jpg',
+	'https://imgur.com/rH5jbGN.jpg',
+	'https://imgur.com/IC50Emf.jpg',
+	'https://imgur.com/z7MDAwk.jpg',
+	'https://imgur.com/UcF1CSC.jpg',
+	'https://imgur.com/kw8cLws.jpg',
+	'https://imgur.com/VXf2QBg.jpg'
+];
+
+var count = 0;
+
 client.on("ready", () => {
 	console.log("Hello World");
 	client.user.setActivity('submarineguns', { type: 'WATCHING' })
@@ -10,9 +27,10 @@ client.on("ready", () => {
 });
 
 var interval = setInterval (function (){
-	    client.channels.cache.get('826133568148602931').send({files: ['https://i.imgur.com/Tivrl2P.jpg']})
+	    client.channels.cache.get('826133568148602931').send({files: [images[count]]})
 	.then(message => console.log(`Sent submarinegun`))
   .catch(console.error);
-}, 5000);
+  count = count + 1;
+}, 86400000);
 
 client.login(process.env.BOT_TOKEN);
---
+++ src/images.js
@@ -0,0 +1,6 @@
+var images = [
+https://imgur.com/V8eAOiL,
+https://imgur.com/juYjN6Y
+];
+
+require('./bot.js')(images);
