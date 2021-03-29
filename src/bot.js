283d076a58265f754eaec090b7c787e9c4f4e8f0
Remlej9 <Remlej9@gmail.com>
2021-03-29 19:00:57 +0200

    Update bot.js

--- src/bot.js
+++ src/bot.js
@@ -7,11 +7,16 @@ client.on("ready", () => {
 	client.user.setActivity('submarineguns', { type: 'WATCHING' })
   .then(presence => console.log(`Activity set to ${presence.activities[0].name}`))
   .catch(console.error);
-    client.channels.cache.get('826133568148602931').send({
+  var guild = client.guilds.get('590635305137078287');
+  if(guild && guild.channels.get('channelid')){
+		    guild.channels.cache.get('826133568148602931').send({
 		files: ['https://i.imgur.com/Tivrl2P.jpg']
 	})
 	.then(message => console.log(`Sent submarinegun`))
   .catch(console.error);
+	} else {
+		console.log("nope");
+	}
 });
 
 client.login(process.env.BOT_TOKEN);
